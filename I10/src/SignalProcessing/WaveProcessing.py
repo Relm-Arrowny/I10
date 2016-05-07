f9af0c206c9f5b6aef1a539f5140db1d35f42fbb
Relm <ferimons@hotmail.com>
2016-05-07 19:02:35 +0100

    Renamed playground to Testlockin and added TestLowPass.
    Added lowpassFilter and fftLowpassFilter

--- I10/src/SignalProcessing/WaveProcessing.py
+++ I10/src/SignalProcessing/WaveProcessing.py
@@ -3,14 +3,12 @@ Created on 7 May 2016
 
 @author: Relm
 
-
 Simple waveform processing include functions:
 
 waveGen ---> generate wave
-hilberTran ----> hilber transformation
+hilbertTran ----> hilbert transformation
 lockin ----> Lockin amplifier
 half/doubleFrequency ----> half or double frequency of wave
-
 
 '''
 import numpy as np
@@ -20,17 +18,19 @@ class WaveProcessing:
     def __init__(self):
         pass
     def waveGen(self, nDataPoint = 10000, time = 10.0, amp = 10.5, freq = 9.0, dc = 0,phase=0):
+        '''Generate and return time and sin wave'''
         k = np.array(np.arange(nDataPoint))
         t = time/nDataPoint*k
         y = dc + amp*np.sin(2*np.pi*freq*t+phase*np.pi/360.)
         return t , y
-    
     def hilbertTran(self,wav):
+        '''Hilber transformation for any real wave'''
         wavefft = np.fft.fft(wav)*2.0
         wavefft[0:len(wav)/2] = 0.0
         return np.fft.ifft(wavefft)
     
     def lockin(self,wave,waveRef):
+        '''Lockin amplifier return R  and phase '''
         x = wave*waveRef
         y = wave*np.imag(self.hilbertTran(waveRef))
         R = (np.average(x)**2+np.average(y)**2)**0.5*2.0
@@ -38,6 +38,7 @@ class WaveProcessing:
         return R, phase
     
     def halfFrequency(self,wav):
+        '''half the frequency of the input wave'''
         y = []
         n = len(wav)
         for i in range(0,n/2):
@@ -46,6 +47,7 @@ class WaveProcessing:
         return y
     
     def doubleFrequency(self,wav):
+        '''double the frequency of the input wave'''
         y = []
         n = len(wav)
         for i in range(0,n,2):
@@ -53,3 +55,22 @@ class WaveProcessing:
         for k in range(n-1,0,-2):
             y.append(-wav[k])
         return y    
+    def lowpassFilter(self,x,dt,RC):
+        y = x
+        a = dt/(RC+dt)
+        for i in range (1, len(y)):
+            y[i]=a *x[i]+(1-a)*y[i-1]
+        return y
+    def fftLowpassFilter(self, x, frqCutoff, dtime):
+        fftresult = np.fft.rfft(x)
+        fftfrq = np.fft.rfftfreq(len(x), dtime)
+        #print fftfrq,fftresult
+        for i in range (0,len(fftfrq)):
+            if fftfrq[i]>frqCutoff:
+                fftresult [i:len(fftresult)] = 0.0
+                return abs(np.fft.irfftn(fftresult))
+                
+        
+        
+        
+        
---
+++ I10/test/TestLowPass.py
@@ -0,0 +1,23 @@
+'''
+Created on 7 May 2016
+
+@author: Relm
+'''
+import numpy as np
+import matplotlib.pyplot as plt
+from SignalProcessing.WaveProcessing import WaveProcessing
+
+wav = wav = WaveProcessing().waveGen()
+for i in range (0,1000):
+    twave =   WaveProcessing().waveGen(10000, 10.0, np.random.random()*1.0+1.0, np.random.random()*500.0,
+                                        np.random.random()*10.0, np.random.random()*360)
+    wav = np.add(twave , wav)
+plt.subplot(211)
+plt.plot(wav[0],wav[1])
+
+wavLowpass = WaveProcessing().fftLowpassFilter(wav[1], 0, 10.0/10000.0)
+
+plt.subplot(212)
+plt.plot(wavLowpass)
+print np.average(wav[1])
+plt.show()
